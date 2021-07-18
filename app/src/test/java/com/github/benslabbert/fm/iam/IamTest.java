47bc6143f53c5c22d8b9c89eaad28de8ca7f3180
benjamin.slabert <benjamin.slabert@gmail.com>
2021-07-18 15:32:39 +0200

    static import

--- app/src/test/java/com/github/benslabbert/fm/iam/IamTest.java
+++ app/src/test/java/com/github/benslabbert/fm/iam/IamTest.java
@@ -1,10 +1,11 @@
 package com.github.benslabbert.fm.iam;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import io.grpc.ManagedChannelBuilder;
 import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import javax.inject.Inject;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 @MicronautTest
@@ -14,13 +15,13 @@ class IamTest {
 
   @Test
   void testItWorks() {
-    Assertions.assertTrue(application.isRunning());
+    assertTrue(application.isRunning());
 
     var channel = ManagedChannelBuilder.forAddress("localhost", 50052).usePlaintext().build();
     var request = com.github.benslabbert.fm.iam.IamRequest.newBuilder().setName("test").build();
     var reply = com.github.benslabbert.fm.iam.IamServiceGrpc.newBlockingStub(channel).send(request);
 
-    Assertions.assertNotNull(reply);
-    Assertions.assertNotNull(reply.getMessage());
+    assertNotNull(reply);
+    assertNotNull(reply.getMessage());
   }
 }
